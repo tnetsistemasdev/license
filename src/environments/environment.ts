49bcd1fea0b6b57c4784e3c7ba601323f08fb09c
luanmarcos <luanmarcosfgns@gmail.com>
2023-02-22 18:12:33 -0400

    PRIMEIRA VERSÃO ESTAVEL DO SISTEMA COM PAGAMENTO
    Took 4 minutes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production:false,
-  API:"http://localhost:8000",
+  API:"http://gestor.tnetsistemas.com.br",
   PUBLIC_KEY_MERCADO_PAGO:"TEST-1bf00641-345e-4a31-b990-e784860e31b3",
   PRIVATE_KEY_MERCADO_PAGO:"TEST-6594797941829024-102818-4b60ebc6de5d4f33df4bddf0650eae46-178658791",
-  URL_SYSTEM: "http://spc.localhost:8765"
+  URL_SYSTEM: "https://spc.tnetsistemas.com.br"
 };
