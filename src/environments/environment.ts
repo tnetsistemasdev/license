af03c693496c872229e9947f67ee9ac491144c83
luanmarcos <luanmarcosfgns@gmail.com>
2023-03-02 17:12:27 -0400

    correção de credencias erradas
    Took 2 minutes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production:false,
   API:"https://gestor.tnetsistemas.com.br",
-  PUBLIC_KEY_MERCADO_PAGO:" APP_USR-e8fed8a2-359a-498c-a0a4-9b4ee81b1d00",
+  PUBLIC_KEY_MERCADO_PAGO:"APP_USR-e8fed8a2-359a-498c-a0a4-9b4ee81b1d00",
   PRIVATE_KEY_MERCADO_PAGO:"APP_USR-8721945862983542-052408-79cb315e886019cc507e85d8882e8c92-1125983327",
 
 };
