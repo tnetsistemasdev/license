9cbc1c3a02dff74ad79f89b715e4c490093f30ec
luanmarcos <luanmarcosfgns@gmail.com>
2023-03-02 16:23:18 -0400

    CORREÇÃO REALIZADO NO CARTÃO
    Took 1 minute

--- dist/license/assets/js/mercadoPagoCard.js
+++ dist/license/assets/js/mercadoPagoCard.js
@@ -1,5 +1,4 @@
-
-setTimeout(()=>{
+setTimeout(() => {
   let public_key = document.getElementById('public_key').value;
   let amount = document.getElementById('amount').value;
   let product = JSON.parse(document.getElementById('product').value);
@@ -70,9 +69,10 @@ setTimeout(()=>{
           identificationNumber,
           identificationType,
         } = cardForm.getCardFormData();
+
         let body = JSON.stringify({
-          contact_id:contact.id,
-          product_id:product.product_id,
+          contact_id: contact.id,
+          product_id: product.product_id,
           token,
           issuer_id,
           payment_method_id,
@@ -87,7 +87,7 @@ setTimeout(()=>{
             },
           },
         });
-        fetch(api_host_gestor+"/api/v2/subscription/pay-card", {
+        fetch(api_host_gestor + "/api/v2/subscription/pay-card", {
           method: "POST",
           headers: {
             "token": "ZmluYW5jZWlyby0wMTpDZXJ0aWZpY2FkbzEyMw==",
@@ -97,13 +97,16 @@ setTimeout(()=>{
         }).then(result => result.json())
           .then((response) => {
             if (response.success == 1) {
-              liberarLicencaCard(product.unit,product.product_id,contact.id)
+              liberarLicencaCard(product.unit, product.product_id, contact.id)
 
-            }else{
+            } else {
               alert(response.message);
+              habilitarButtonPagamento();
             }
-          }).catch(function (error){
-            console.log(error);
+          }).catch(function (error) {
+          console.log(error);
+          alert('Houve um erro no servidor');
+          habilitarButtonPagamento();
         })
       },
       onFetching: (resource) => {
@@ -111,12 +114,19 @@ setTimeout(()=>{
       }
     },
   });
-},1500);
+}, 1500);
 
-function liberarLicencaCard(unidade, product_id,contact_id) {
+function habilitarButtonPagamento() {
+  document.querySelector('#form-checkout__submit').removeAttribute('disabled');
+  document.querySelector('#loading-spinner').classList.add('d-none');
+  document.querySelector('#form-checkout__submit').classList.remove('disabled');
+}
+
+function liberarLicencaCard(unidade, product_id, contact_id) {
   let api_host_gestor = document.getElementById('api_host').value;
   let days = 0;
-  if (unidade == 'UNID' || unidade == 'DIA') {
+  let number_units = 0;
+  if (unidade == 'DIA') {
     days = 1;
   } else if (unidade == 'SEMANA') {
     days = 7;
@@ -129,12 +139,14 @@ function liberarLicencaCard(unidade, product_id,contact_id) {
   } else if (unidade == 'VITAL') {
     days = 36500;
   } else {
-    console.log('UNIDADE INVALIDA');
+    number_units = 1;
   }
+
   let body = JSON.stringify({
     "contact_id": contact_id,
     "product_id": product_id,
-    "days": days
+    "days": days,
+    "number_units": number_units
   });
   fetch(api_host_gestor + '/api/v2/license/create', {
     body: body,
@@ -142,17 +154,53 @@ function liberarLicencaCard(unidade, product_id,contact_id) {
       "token": "ZmluYW5jZWlyby0wMTpDZXJ0aWZpY2FkbzEyMw==",
       "Content-Type": "application/json; charset=UTF-8"
     },
-    method:"POST"
-  }) .then(result => result.json())
-    .then((response) => {
-    if (response.success) {
-      alert('Pagamento Realizado');
-      window.location.href = document.getElementById('url_system').value;
-    } else {
-      alert(response.message);
-      console.log(response);
+    method: "POST"
+  }).then(result => result.json()).then((response) => {
+      if (response.success) {
+        alert('Pagamento Realizado');
+        let location_app = atob(document.getElementById('url_system').value);
+        //se a cobrança for por tempos ele redireciona
+        if (days > 0) {
+
+          window.location.href = location_app;
+        }
+
+        //se for unidade
+          if (number_units == 1) {
+
+          let body = JSON.stringify({
+            "contact_id": contact_id,
+            "product_id": product_id
+          });
+
+          fetch(location_app + '/api/v2/webhook', {
+            body: body,
+            headers: {
+              "token": "ZmluYW5jZWlyby0wMTpDZXJ0aWZpY2FkbzEyMw==",
+              "Content-Type": "application/json"
+            },
+            method: "POST"
+          }).then(result => result.json())
+            .then((response) => {
+              console.log(response);
+              if (response.success == true) {
+                window.history.back();
+              } else {
+                this.toastr.error(response.message);
+                console.log(response);
+              }
+
+            });
+
+
+        }
+      } else {
+        alert(response.message);
+        habilitarButtonPagamento();
+        console.log(response);
+      }
     }
-  }).catch((error) => {
+  ).catch((error) => {
     console.log(error);
   });
 }
